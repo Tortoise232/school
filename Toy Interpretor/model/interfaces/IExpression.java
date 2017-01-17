fcb043d301da7cc19f0027a80c2fc971ecd8f30b
Tortoise232 <petean.mihai232@yahoo.com>
2017-01-17 09:08:36 +0200

    moved toy interpretor
    added lisp practice

--- Toy Interpretor/model/interfaces/IExpression.java
+++
@@ -1,9 +0,0 @@
-package model.interfaces;
-import java.io.Serializable;
-
-import model.interfaces.IDictionary;
-public interface IExpression extends Serializable{
-		public int evaluate(IDictionary<String,Integer> myDic, IHeap myHeap);
-}
-
-
--- Toy Interpretor/model/interfaces/IFileTable.java
+++
@@ -1,7 +0,0 @@
-package model.interfaces;
-
-import java.io.Serializable;
-
-public interface IFileTable extends Serializable{
-	
-}
